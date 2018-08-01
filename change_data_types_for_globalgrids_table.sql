03d3ace18c0fb76f600e628f03236e4f74a2a703
danallen <danallen@awhere.com>
2018-08-01 12:57:56 -0400

    wt4-3475: Fix mismatched data types within the location sets schema

---
+++ change_data_types_for_globalgrids_table.sql
@@ -0,0 +1,4 @@
+alter table public.globalgrids alter column locationid type int;
+alter table public.globalgrids alter column latitude type decimal(9,6);
+alter table public.globalgrids alter column longitude type decimal(9,6);
+alter table public.globalgrids alter column elevation type int;
